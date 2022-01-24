a3772325c8b3049a39f703cdf6129f9c1c44159e
mitya_me <dmitrymefodyev@gmail.com>
2022-01-24 08:46:55 +0300

    Add GlobalStyle

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -11,27 +11,39 @@ export const GlobalStyle = createGlobalStyle`
 	:root {
 		--ff-prime: 'Helvetica Neue', sans-serif;
 
-		--fs-s: 16px;
-		--fs-m: 18px;
-		
-		--fw-s: 600;
-		--fw-l: 800; 
 
-		--lh-s: 19px;
-		--lh-m: 22px;
+		--fz-s: 14px;
+		--fz-m: 16px;
+		--fz-l: 18px;
+		--fz-xl: 40px;
+		--fz-bigger: 64px;
 
+		--fw-s: 400;
+		--fw-m: 700; 
+		--fw-l: 800;
+
+		--lh-s: 17px;
+		--lh-m: 19px;
+		--lh-l: 22px;
+		--lh-xl: 48px;
+		--lh-bigger: 78px;
+
+		--clr-text: #000000;
 		--clr-bg: #E5E5E5;
 		--clr-input-bg: #F5F5F5;
 		--clr-btn-prime: #4A67FF;
 		--clr-btn-prime-hover: #3355FF;
+		--clr-btn-prime-dis: #99A9FF;
+		--clr-error: #E26F6F;
+		--clr-error-bg: #F5E9E9;
 
 		--radius: 8px;
 	}
 
 	body {
 		font-family: var(--ff-prime);
-		font-size: var(--fs-s);
-		line-height: var(--lh-s);
+		font-size: var(--fz-m);
+		line-height: var(--lh-l);
 	}
 
 	input,
